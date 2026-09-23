02b0b84e2537abd44f7ac5e8e58efc9249e42427
Sam <ngugindunguapps@gmail.com>
2026-09-23 08:40:25 +0300

    Add custom validator for image content type

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,16 @@ class Product < ApplicationRecord
   has_one_attached :image
   validates :price, numericality: { greater_than_or_equal_to: 0.01}
   validates :title uniqueness: true
+  validate :acceptable_image
   validates :title, :description, :image, presence: true
   after_commit -> { broadcast_refresh_later_to "products"}
+
+
+  def acceptable_image
+    return unless image.attached?
+    acceptable_types = ["image/jpeg", "image/png", "image/gif"]
+
+    unless acceptable_types.include?(image.content_type)
+      errors. add ( :image , "must be a GIF, JPG or PNG image")
+    end
 end
