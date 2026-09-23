3d938d3b9d078343d6df09e991df4666b1930e73
Sam <ngugindunguapps@gmail.com>
2026-09-23 08:19:27 +0300

    Require product fields to be non-empty using validates/required

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  validates :title, :description, :image, presence: true
   after_commit -> { broadcast_refresh_later_to "products"}
 end
