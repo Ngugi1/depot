e5e604462daaeee8564ffae1901b5b85efefe45b
Sam <ngugindunguapps@gmail.com>
2026-09-23 08:46:36 +0300

    Enforce expected image content type in model - the form can be bypassed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image
   validates :price, numericality: { greater_than_or_equal_to: 0.01}
-  validates :title uniqueness: true
+  validates :title, uniqueness: true
   validate :acceptable_image
   validates :title, :description, :image, presence: true
   after_commit -> { broadcast_refresh_later_to "products"}
@@ -12,6 +12,7 @@ class Product < ApplicationRecord
     acceptable_types = ["image/jpeg", "image/png", "image/gif"]
 
     unless acceptable_types.include?(image.content_type)
-      errors. add ( :image , "must be a GIF, JPG or PNG image")
+      errors.add(:image,"must be a GIF, JPG or PNG image")
     end
+  end
 end
