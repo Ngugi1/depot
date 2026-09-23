e87bf19ba84f3801fe07be1511221193bdb817e8
Sam <ngugindunguapps@gmail.com>
2026-09-23 08:23:40 +0300

    Ensure price is numerical and greater or equal to 1 cent

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  validates :price, numericality: { greater_than_or_equal_to: 0.01}
   validates :title, :description, :image, presence: true
   after_commit -> { broadcast_refresh_later_to "products"}
 end
